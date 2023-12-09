db98fea93cc5ce658a681c5c24fd87dc67f49bf6
Ingo van Lil <inguin@gmx.de>
2023-12-09 07:32:55 +0100

    Solutions for day 9: Mirage Maintenance

---
+++ day9/mirage1.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+import sys, re
+
+def extrapolate(seq):
+    diffs = [ b - a for a, b in zip(seq[:-1], seq[1:]) ]
+    if all(diff == 0 for diff in diffs):
+        return seq[-1]
+    return seq[-1] + extrapolate(diffs)
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+total = 0
+
+with open(filename) as f:
+    for line in f:
+        seq = [ int(m) for m in line.split() ]
+        val = extrapolate(seq)
+        print(val)
+        total += val
+
+print(total)
